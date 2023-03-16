3e9c608221e8e00eb93b06d3526867d7e58193fb
Melvin Carvalho <melvincarvalho@gmail.com>
2023-03-17 00:43:57 +0100

    add ledgr.json

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-export default class Ledgr {
+// ledger.js
+import fs from 'fs';
+
+export default class Ledger {
   constructor() {
-    this.balances = {};
+    this.filename = 'ledgr.json';
+    this.balances = this.loadBalances();
+  }
+
+  loadBalances() {
+    try {
+      const data = fs.readFileSync(this.filename, 'utf-8');
+      return JSON.parse(data);
+    } catch (error) {
+      // If the file doesn't exist or is empty, return an empty object.
+      return {};
+    }
+  }
+
+  saveBalances() {
+    const data = JSON.stringify(this.balances, null, 2);
+    fs.writeFileSync(this.filename, data, 'utf-8');
   }
 
   deposit(public_key, amount) {
@@ -9,11 +28,13 @@ export default class Ledgr {
     } else {
       this.balances[public_key] = amount;
     }
+    this.saveBalances();
   }
 
   withdraw(public_key, amount) {
     if (this.balances[public_key] && this.balances[public_key] >= amount) {
       this.balances[public_key] -= amount;
+      this.saveBalances();
     } else {
       console.error("Insufficient balance or invalid public key.");
     }
